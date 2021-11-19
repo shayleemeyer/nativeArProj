da59f80671478b9b7492c5e421f7fc7ad28b0780
Shaylee Meyer <shayleeomeyer@gmail.com>
2021-11-19 09:41:27 -0600

    updates

--- App.js
+++ App.js
@@ -9,12 +9,11 @@ import {
   View,
 } from "react-native";
 
-
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <TouchableWithoutFeedback onPress={() =>}>
+      <TouchableWithoutFeedback>
         <Image
           source={{
             width: 200,
